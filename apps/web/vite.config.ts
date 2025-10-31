aa417db187d2442ef5c20624f7968b58e8e05a21
PaulSenon <paul.senon@gmail.com>
2025-10-31 22:33:05 +0100

    add shadcn defaults and ai elements

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,27 +2,27 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
-import alchemy from "alchemy/cloudflare/vite";
+// import alchemy from "alchemy/cloudflare/vite";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tanstackRouter({}),
     react(),
-    alchemy(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "ai-monorepo",
-        short_name: "ai-monorepo",
-        description: "ai-monorepo - PWA Application",
-        theme_color: "#0c0c0c",
-      },
-      pwaAssets: { disabled: false, config: true },
-      devOptions: { enabled: true },
-    }),
+    // alchemy(), // this breaks everything
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   manifest: {
+    //     name: "ai-monorepo",
+    //     short_name: "ai-monorepo",
+    //     description: "ai-monorepo - PWA Application",
+    //     theme_color: "#0c0c0c",
+    //   },
+    //   pwaAssets: { disabled: false, config: true },
+    //   devOptions: { enabled: true },
+    // }),
     // Inspect(),
     // analyzer({
     //   openAnalyzer: false,
