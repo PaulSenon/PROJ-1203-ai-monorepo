4500fb2312d7914e12575dc4991519baed963001
PaulSenon <paul.senon@gmail.com>
2026-02-09 00:26:41 +0900

    committed some old temp things

--- apps/web/src/lib/convex/queries.ts
+++ apps/web/src/lib/convex/queries.ts
@@ -299,84 +299,84 @@ const convexMutations = {
         }
       );
     }),
-  upsertThread: () =>
-    useCvxMutationAuth(api.chat.upsertThread).withOptimisticUpdate(
-      (localStore, mutationArgs) => {
-        // thread query
-        const threadQuery = convexQueries.getThread({
-          threadUuid: mutationArgs.threadUuid,
-        });
-        const thread1 = localStore.getQuery(
-          threadQuery.query,
-          ...threadQuery.args
-        );
-        localStore.setQuery(threadQuery.query, ...threadQuery.args, {
-          _id: thread1?._id ?? (crypto.randomUUID() as Id<"threads">),
-          _creationTime: thread1?._creationTime ?? Date.now(),
-          userId: thread1?.userId ?? (crypto.randomUUID() as Id<"users">),
-          uuid: mutationArgs.threadUuid,
-          createdAt: thread1?.createdAt ?? Date.now(),
-          updatedAt: Date.now(),
-          lifecycleState: thread1?.lifecycleState ?? "active",
-          liveStatus:
-            mutationArgs.patch.liveStatus ?? thread1?.liveStatus ?? "completed",
-          title: mutationArgs.patch.title ?? thread1?.title,
-          deletedAt: thread1?.deletedAt,
-          lastUsedModelId:
-            mutationArgs.patch.lastUsedModelId ?? thread1?.lastUsedModelId,
-        });
+  // upsertThread: () =>
+  //   useCvxMutationAuth(api.chat.upsertThread).withOptimisticUpdate(
+  //     (localStore, mutationArgs) => {
+  //       // thread query
+  //       const threadQuery = convexQueries.getThread({
+  //         threadUuid: mutationArgs.threadUuid,
+  //       });
+  //       const thread1 = localStore.getQuery(
+  //         threadQuery.query,
+  //         ...threadQuery.args
+  //       );
+  //       localStore.setQuery(threadQuery.query, ...threadQuery.args, {
+  //         _id: thread1?._id ?? (crypto.randomUUID() as Id<"threads">),
+  //         _creationTime: thread1?._creationTime ?? Date.now(),
+  //         userId: thread1?.userId ?? (crypto.randomUUID() as Id<"users">),
+  //         uuid: mutationArgs.threadUuid,
+  //         createdAt: thread1?.createdAt ?? Date.now(),
+  //         updatedAt: Date.now(),
+  //         lifecycleState: thread1?.lifecycleState ?? "active",
+  //         liveStatus:
+  //           mutationArgs.patch.liveStatus ?? thread1?.liveStatus ?? "completed",
+  //         title: mutationArgs.patch.title ?? thread1?.title,
+  //         deletedAt: thread1?.deletedAt,
+  //         lastUsedModelId:
+  //           mutationArgs.patch.lastUsedModelId ?? thread1?.lastUsedModelId,
+  //       });
 
-        // paginated query
-        const paginatedQuery = convexQueries.threadHistoryPaginated({});
-        const query = paginatedQuery.query;
-        const args = paginatedQuery.args;
+  //       // paginated query
+  //       const paginatedQuery = convexQueries.threadHistoryPaginated({});
+  //       const query = paginatedQuery.query;
+  //       const args = paginatedQuery.args;
 
-        // we get and filter out the thread if already exist
-        const allPages = localStore.getAllQueries(query);
-        let thread: Doc<"threads"> | undefined;
-        for (const { value, args: _args } of allPages) {
-          if (!value) continue;
-          for (const item of value.page) {
-            if (item.uuid === mutationArgs.threadUuid) {
-              // Replace with your match condition
-              thread = item;
-              localStore.setQuery(query, _args, {
-                ...value,
-                page: value.page.filter(
-                  (i) => i.uuid !== mutationArgs.threadUuid
-                ),
-              });
-              break;
-            }
-          }
-          if (thread) break; // Stop as soon as you find a match
-        }
+  //       // we get and filter out the thread if already exist
+  //       const allPages = localStore.getAllQueries(query);
+  //       let thread: Doc<"threads"> | undefined;
+  //       for (const { value, args: _args } of allPages) {
+  //         if (!value) continue;
+  //         for (const item of value.page) {
+  //           if (item.uuid === mutationArgs.threadUuid) {
+  //             // Replace with your match condition
+  //             thread = item;
+  //             localStore.setQuery(query, _args, {
+  //               ...value,
+  //               page: value.page.filter(
+  //                 (i) => i.uuid !== mutationArgs.threadUuid
+  //               ),
+  //             });
+  //             break;
+  //           }
+  //         }
+  //         if (thread) break; // Stop as soon as you find a match
+  //       }
 
-        // we insert at top, either the same thread patched, ot a new one
-        insertAtTop({
-          paginatedQuery: query,
-          argsToMatch: args, // same args as in the sidebar
-          localQueryStore: localStore,
-          item: {
-            _id: thread?._id ?? (crypto.randomUUID() as Id<"threads">),
-            _creationTime: thread?._creationTime ?? Date.now(),
-            userId: thread?.userId ?? (crypto.randomUUID() as Id<"users">),
-            uuid: mutationArgs.threadUuid,
-            createdAt: thread?.createdAt ?? Date.now(),
-            updatedAt: Date.now(),
-            lifecycleState: thread?.lifecycleState ?? "active",
-            liveStatus:
-              mutationArgs.patch.liveStatus ??
-              thread?.liveStatus ??
-              "completed",
-            title: mutationArgs.patch.title ?? thread?.title,
-            lastUsedModelId:
-              mutationArgs.patch.lastUsedModelId ?? thread?.lastUsedModelId,
-            deletedAt: thread?.deletedAt,
-          },
-        });
-      }
-    ),
+  //       // we insert at top, either the same thread patched, ot a new one
+  //       insertAtTop({
+  //         paginatedQuery: query,
+  //         argsToMatch: args, // same args as in the sidebar
+  //         localQueryStore: localStore,
+  //         item: {
+  //           _id: thread?._id ?? (crypto.randomUUID() as Id<"threads">),
+  //           _creationTime: thread?._creationTime ?? Date.now(),
+  //           userId: thread?.userId ?? (crypto.randomUUID() as Id<"users">),
+  //           uuid: mutationArgs.threadUuid,
+  //           createdAt: thread?.createdAt ?? Date.now(),
+  //           updatedAt: Date.now(),
+  //           lifecycleState: thread?.lifecycleState ?? "active",
+  //           liveStatus:
+  //             mutationArgs.patch.liveStatus ??
+  //             thread?.liveStatus ??
+  //             "completed",
+  //           title: mutationArgs.patch.title ?? thread?.title,
+  //           lastUsedModelId:
+  //             mutationArgs.patch.lastUsedModelId ?? thread?.lastUsedModelId,
+  //           deletedAt: thread?.deletedAt,
+  //         },
+  //       });
+  //     }
+  //   ),
 } as const;
 
 export const cvx = {
